b206963be09fbeb6342546e0c9e4578f42613fea
mineduck1608 <ducdm160804@gmail.com>
2024-06-29 14:36:45 +0700

    1

---
+++ SportunifyForm/HomeScreen.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SportunifyForm
+{
+    /// <summary>
+    /// Interaction logic for HomeScreen.xaml
+    /// </summary>
+    public partial class HomeScreen : Window
+    {
+        public HomeScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
