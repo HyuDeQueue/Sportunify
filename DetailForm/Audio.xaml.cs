4dc1aec51bb7d9715038c5ac539b81a2e226a23e
Anhnguyenanh123 <138548969+Anhnguyenanh123@users.noreply.github.com>
2024-07-18 02:49:01 +0700

    Add files

---
+++ DetailForm/Audio.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DetailForm
+{
+    /// <summary>
+    /// Interaction logic for Audio.xaml
+    /// </summary>
+    public partial class Audio : Window
+    {
+        public Audio()
+        {
+            InitializeComponent();
+        }
+
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        string fileName;
+
+        private void BT_Click_Play(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Play();
+        }
+
+        private void BT_Click_Pause(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Pause();
+        }
+
+        private void BT_Click_Stop(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+        }
+
+        private void BT_Click_Open(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog
+            {
+                Multiselect = false,
+                DefaultExt = ".mp3"
+            };
+            bool? diaLogOK = fileDialog.ShowDialog();
+            if (diaLogOK == true)
+            {
+                fileName = fileDialog.FileName;
+                FileName.Text = fileDialog.SafeFileName;
+                mediaPlayer.Open(new Uri(fileName));
+            }
+        }
+    }
+}
---
+++ SportunifyForm/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Authenticators.OAuth2;
+using SportunifyForm.MVVM.Model.QuickType;
+using System.Collections.ObjectModel;
+
+namespace SportunifyForm.MVVM.ViewModel
+{
+    internal class MainViewModel
+    {
+        public ObservableCollection<Item> Songs { get; set; }
+        public MainViewModel()
+        {
+            Songs = new ObservableCollection<Item>();
+            PopulateCollection();
+        }
+
+        void PopulateCollection()
+        {
+            var client = new RestClient();
+            client.Authenticator = new OAuth2AuthorizationRequestHeaderAuthenticator("", "Bearer");
+
+            var request = new RestRequest("https://api.spotify.com/v1/browse/new-releases", Method.Get);
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Content-type", "application/json");
+
+            var response = client.GetAsync(request).GetAwaiter().GetResult();
+
+            var data = JsonConvert.DeserializeObject<TrackModel>(response.Content);
+
+            for (int i = 0; i < data.Albums.Limit; i++)
+            {
+                var Track = data.Albums.Items[i];
+                Track.Duration = "2:32";
+                Songs.Add(Track);
+            }
+
+        }
+
+    }
+}
