6243f8c614d8caffb0493611be166ff90a44bcfe
Hyudeque <quanghuy01062004@gmail.com>
2024-07-19 15:43:10 +0700

    Adding basic account services

--- Repositories/Repositories/AccountRepository.cs
+++ Repositories/Repositories/AccountRepository.cs
@@ -10,5 +10,40 @@ namespace Repositories.Repositories
     public class AccountRepository
     {
         ListentogetherContext _context;
+
+        public Account GetAccount(Account account)
+        {
+            _context = new();
+
+            var result = _context.Accounts
+                .Where(x => x.Username == account.Username && x.Password == account.Password)
+                .FirstOrDefault();
+            return result;
+            
+        }
+
+        public List<Account> GetAllAccounts()
+        {
+            _context = new();
+            var result = _context.Accounts
+                .ToList();
+            return result;
+        }
+        
+        public void CreateAccount(Account account)
+        {
+            _context = new();
+            _context.Accounts.Add(account);
+            _context.SaveChanges();
+        }
+
+        public Account FindAccount(string username)
+        {
+            _context = new();
+            var result = _context.Accounts
+                .Where(x => x.Username == username)
+                .FirstOrDefault();
+            return result;
+        }
     }
 }
--- Services/Services/AccountService.cs
+++ Services/Services/AccountService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Repositories.Models;
+using Repositories.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,24 +10,37 @@ namespace Services.Services
 {
     public class AccountService
     {
+        private AccountRepository _repo = new();
         public void GetAllAccounts()
         {
+            List<Account> accounts = _repo.GetAllAccounts();
+        }
+        public String Register(Account account)
+        {
+            Account isRegistered = _repo.FindAccount(account.Username);
 
+            if (isRegistered is not null)
+            {
+                return("Username already exists!");
+            }
+            else
+            {
+                _repo.CreateAccount(account);
+                return("Account created successfully!");
+            }
         }
 
-        public void GetSelectedAccount()
+        public Account Login(Account account)
         {
-
-        }
-
-        public void Register()
-        {
-
-        }
-
-        public void Login()
-        {
-
+            Account loggedIn = _repo.GetAccount(account);
+            if (loggedIn is not null)
+            { 
+                return loggedIn;
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public void Logout()
