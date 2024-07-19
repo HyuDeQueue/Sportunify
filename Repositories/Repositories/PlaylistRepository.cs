87a2f34f7b5c7aef83deb263a8fe46a506aac50f
Hyudeque <quanghuy01062004@gmail.com>
2024-07-19 15:24:29 +0700

    Adding context

---
+++ Repositories/Repositories/PlaylistRepository.cs
@@ -0,0 +1,14 @@
+﻿using Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Repositories
+{
+    public class PlaylistRepository
+    {
+        ListentogetherContext _context;
+    }
+}
