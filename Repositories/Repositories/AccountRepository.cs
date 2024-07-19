6241fabdf2490ef071cea83be627724757c2ed13
Hyudeque <quanghuy01062004@gmail.com>
2024-07-19 23:38:06 +0700

    Adding Repo for account

--- Repositories/Repositories/AccountRepository.cs
+++ Repositories/Repositories/AccountRepository.cs
@@ -9,6 +9,47 @@ namespace Repositories.Repositories
 {
     public class AccountRepository
     {
-        private ListentogetherContext _context;
+        ListentogetherContext _context;
+
+        public Account GetAccount(Account account)
+        {
+            _context = new();
+
+            var result = _context.Accounts
+                .Where(x => x.Username == account.Username)
+                .FirstOrDefault();
+            if (result != null && BCrypt.Net.BCrypt.Verify(account.Password, result.Password))
+            {
+                return result;
+            }
+
+            return null;
+
+        }
+
+        public List<Account> GetAllAccounts()
+        {
+            _context = new();
+            var result = _context.Accounts
+                .ToList();
+            return result;
+        }
+
+        public void CreateAccount(Account account)
+        {
+            _context = new();
+            account.Password = BCrypt.Net.BCrypt.HashPassword(account.Password);
+            _context.Accounts.Add(account);
+            _context.SaveChanges();
+        }
+
+        public Account FindAccount(string username)
+        {
+            _context = new();
+            var result = _context.Accounts
+                .Where(x => x.Username == username)
+                .FirstOrDefault();
+            return result;
+        }
     }
 }
---
+++ Services/Services/CategoryService.cs
@@ -0,0 +1,31 @@
+﻿using Repositories.Models;
+using Repositories.Repositories;
+using System.Collections.Generic;
+
+namespace Services.Services
+{
+    public class CategoryService
+    {
+        private readonly CategoryRepository _categoryRepository;
+
+        public CategoryService(CategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            return _categoryRepository.GetAllCategories();
+        }
+
+        public void CreateCategory(Category category)
+        {
+            _categoryRepository.CreateCategory(category);
+        }
+
+        public void EditCategory(Category category)
+        {
+            _categoryRepository.EditCategory(category);
+        }
+    }
+}
